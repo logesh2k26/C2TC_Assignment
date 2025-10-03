86136c63d545bd95e56447dc5b09d877df95e392
Logesh D <logesh2k25@gmail.com>
2025-10-04 00:00:37 +0530

    Update MainClass.java

--- src/com/tnsif/assignmenttwo/Commision/MainClass.java
+++ src/com/tnsif/assignmenttwo/Commision/MainClass.java
@@ -2,10 +2,7 @@ package com.tnsif.assignmenttwo.Commision;
 
 public class MainClass {
     public static void main(String[] args) {
-        // Create Student object
         Student s1 = new Student();
-
-        // Create Commission object and test
         Commission emp = new Commission();
         emp.acceptDetails();
         emp.calculateCommission();
