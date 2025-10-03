5a4705862a038cffec052deb0d91b44af9e73bc3
Logesh D <logesh2k25@gmail.com>
2025-10-04 00:01:06 +0530

    Update Student.java

--- src/com/tnsif/assignmenttwo/Commision/Student.java
+++ src/com/tnsif/assignmenttwo/Commision/Student.java
@@ -1,7 +1,6 @@
 package com.tnsif.assignmenttwo.Commision;
 
 public class Student {
-    // Default constructor
     public Student() {
         System.out.println("Student object is created");
     }
